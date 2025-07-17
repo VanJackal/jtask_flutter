912d1c150060c2f3e391ca93f47b46c114eacd66
VanJackal <v@njackal.com>
2025-07-17 18:15:37 -0500

    initial task page setup

---
+++ lib/ui/homepage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:jtask_flutter/ui/tasklist.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Row(
+        children: [
+          Placeholder(),
+          Expanded(child: TaskList()),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/tasklist.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'package:jtask_flutter/types.dart';
+
+/*
+todo
+This may need to be moved away from table to support subtasks 
+ */
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      columnWidths: const <int, TableColumnWidth> {
+        0: FixedColumnWidth(64),
+        1: FlexColumnWidth(1),
+        2: IntrinsicColumnWidth()
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: [
+        TaskItem(task: (title: "Task 1", dueDate: DateTime(2025,1, 25), state: true)),//todo this should be stored in a var
+        TaskItem(task: (title: "Task 2", dueDate: DateTime(2025,6, 2), state: true)),
+      ],
+    );
+  }
+}
+
+class TaskItem extends TableRow {
+  const TaskItem({super.key, super.decoration,required this.task});
+  
+  final Task task;
+
+  @override
+  List<Widget> get children => [
+    Container(height:32, color:Colors.green,),
+    TableCell(
+      child: Text(task.title),
+    ),
+    TableCell(
+      child: Text(task.dueDate.toString()),
+    )
+  ];
+}
