9d5b6e981563dea69858bb8c0acd45d151f6c132
jackal <v@njackal.com>
2025-09-03 16:41:26 -0500

    replaced tasklist table with just a list

--- lib/ui/tasklist/taskList_view.dart
+++ lib/ui/tasklist/taskList_view.dart
@@ -33,18 +33,7 @@ class TaskList extends StatelessWidget {
             ),
             Expanded(
               child: ListView(
-                children: [
-                  Table(
-                    columnWidths: const <int, TableColumnWidth> {
-                      0: FixedColumnWidth(64),
-                      1: FlexColumnWidth(1),
-                      2: IntrinsicColumnWidth(),
-                      3: IntrinsicColumnWidth()
-                    },
-                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                    children: tasks,
-                  ),
-                ]
+                  children: tasks
               ),
             ),
           ],
@@ -54,41 +43,49 @@ class TaskList extends StatelessWidget {
   }
 }
 
-class TaskItem extends TableRow {
-  const TaskItem({super.key, super.decoration,required this.task, required this.viewModel, required this.context});
+class TaskItem extends StatelessWidget {
+  const TaskItem({super.key, required this.task, required this.viewModel, required this.context});
   
   final Task task;
   final TaskListViewModel viewModel;
   final BuildContext context;
 
-  @override
-  List<Widget> get children => [
-    GestureDetector(
-      onTap: (){viewModel.setDone(task.id, !task.state);},
-      child: Container(
-        height:32, 
-        color: task.state? Colors.green:Colors.red,
-        child:Icon(task.state ? Icons.check_circle_outline : Icons.circle_outlined )
-      ),
-    ),
-    TableCell(
-      child: Text(task.title),
-    ),
-    TableCell(
-      child: Text(task.dueDate.toString()),
-    ),
-    GestureDetector(
-      onTap: (){
-        showDialog(context: context, builder: (BuildContext context) => Dialog(
-          child: EditTaskDialog(initTask:task, viewModel: viewModel,)
-        ));
-      },
-      child: Icon(Icons.edit),
-      
-    )
-  ];
-
   static List<TaskItem> from(List<Task> tasks, TaskListViewModel viewModel, BuildContext context) {
     return tasks.map((t)=> TaskItem(task:t, viewModel: viewModel, context: context)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: (){viewModel.setDone(task.id, !task.state);},
+          child: SizedBox(
+              height:32,
+              child:Icon(task.state ? Icons.check_circle_outline : Icons.circle_outlined )
+          ),
+        ),
+        Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(task.title),
+            )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(task.dueDate.toString()),
+        ),
+        GestureDetector(
+          onTap: (){
+            showDialog(context: context, builder: (BuildContext context) => Dialog(
+                child: EditTaskDialog(initTask:task, viewModel: viewModel,)
+            ));
+          },
+          child: Icon(Icons.edit),
+
+        )
+
+      ],
+    );
   }
 }
